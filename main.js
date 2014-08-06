01c6de8ceac3b1fcf0a63b84e9716abaa62f03f6
Joel Jensen <yobert@gmail.com>
2014-08-06 16:17:08 -0600

    Add support for STDIN/STDOUT

--- main.js
+++ main.js
@@ -9,13 +9,28 @@ var infile = process.argv[2];
 var outfile = process.argv[3];
 
 if(!infile || !outfile) {
-	throw('input and output file path must be passed');
+	throw('input and output file path must be passed (you can use - for STDIN/STDOUT)');
 }
 
 var header = '/** @jsx React.DOM */' + "\n";
-var source = header + fs.readFileSync(infile, "utf8");
+var source = header;
 
-source = rt.transform(source);
+var write = function() {
+	source = rt.transform(source);
 
-fs.writeFileSync(outfile, source);
+	if(outfile != '-') {
+		fs.writeFileSync(outfile, source);
+	} else {
+		process.stdout.write(source);
+	}
+};
+
+if(infile != '-') {
+	source += fs.readFileSync(infile, "utf8");
+	write();
+} else {
+	process.stdin.resume();
+	process.stdin.on('data', function(buf) { source += buf.toString(); });
+	process.stdin.on('end', write);
+}
 
